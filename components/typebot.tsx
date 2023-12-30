e8efee0ca15997accdf1ac91aace3968d36a8964
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-12-30 14:02:02 +0530

    refactor: typebot appearance

--- components/typebot.tsx
+++ components/typebot.tsx
@@ -3,13 +3,12 @@ import { Bubble } from "@typebot.io/nextjs";
 export const TypeBot = () => {
   return (
     <Bubble
-      typebot="lead-generation-aq9b321"
+      typebot="antonyjudeshaman"
       theme={{
         button: {
-          backgroundColor: "#4A8BB2",
+          backgroundColor: "blue",
           customIconSrc:
-            "https://s3.typebot.io/public/workspaces/cllhhsppt0005l10ff976ti47/typebots/cllhhvlg1002cla0fuaq9b321/bubble-icon?v=1703921886411",
-          size: "large",
+            "https://s3.typebot.io/public/workspaces/cllhhsppt0005l10ff976ti47/typebots/cllhhvlg1002cla0fuaq9b321/hostAvatar?v=1703924007799",
         },
       }}
     />
