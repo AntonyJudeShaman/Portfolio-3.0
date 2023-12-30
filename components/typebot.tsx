d511602680231fa68b68e584cb770b0e9cabfa16
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-12-30 13:12:35 +0530

    add: typebot integration

---
+++ components/typebot.tsx
@@ -0,0 +1,17 @@
+import { Bubble } from "@typebot.io/nextjs";
+
+export const TypeBot = () => {
+  return (
+    <Bubble
+      typebot="lead-generation-aq9b321"
+      theme={{
+        button: {
+          backgroundColor: "#4A8BB2",
+          customIconSrc:
+            "https://s3.typebot.io/public/workspaces/cllhhsppt0005l10ff976ti47/typebots/cllhhvlg1002cla0fuaq9b321/bubble-icon?v=1703921886411",
+          size: "large",
+        },
+      }}
+    />
+  );
+};
